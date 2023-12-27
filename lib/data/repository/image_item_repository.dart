18b06b887d343d6969a85a5c7243ea6e4ccd72b8
shin-SeungHun <sng7387@gmail.com>
2023-12-27 23:10:22 +0900

    데이터 가져와서 hits의 데이터를 mapper로 바꾸고 리스트로 변환해 리턴

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -1,4 +1,7 @@
 import 'package:image_search_app/data/data_source/pixabay_api.dart';
+import 'package:image_search_app/data/mapper/image_mapper.dart';
+
+import '../dto/pixabay_dto.dart';
 import '../model/image_item.dart';
 
 abstract interface class ImageItemRepository {
@@ -9,10 +12,13 @@ class PixabayImageItemRepository implements ImageItemRepository {
   final PixabayApi _api = PixabayApi();
 
   @override
-  Future<List<ImageItem>> getImageItem(String query) {
-    // TODO: implement getImageItem
-    throw UnimplementedError();
+  Future<List<ImageItem>> getImageItem(String query) async {
+    final PixabayDto dto = await _api.getImagesResult(query);
+
+    if (dto.hits == null) {
+      return [];
+    }
+
+    return dto.hits!.map((e) => e.toImageItem()).toList();
   }
-
-
 }
