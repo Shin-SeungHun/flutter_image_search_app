9e2bd8fe17f7930e8936ed7588ab3243a5105f8a
shin-SeungHun <sng7387@gmail.com>
2023-12-28 01:24:00 +0900

    repository 클래스 분리

--- lib/data/repository/pixabay_image_item_repository.dart
+++ lib/data/repository/pixabay_image_item_repository.dart
@@ -1,18 +1,15 @@
-import 'package:image_search_app/data/data_source/pixabay_api.dart';
 import 'package:image_search_app/data/mapper/image_mapper.dart';
 
+import '../data_source/pixabay_api.dart';
 import '../dto/pixabay_dto.dart';
 import '../model/image_item.dart';
-
-abstract interface class ImageItemRepository {
-  Future<List<ImageItem>> getImageItem(String query);
-}
+import 'interface/image_item_repository.dart';
 
 class PixabayImageItemRepository implements ImageItemRepository {
   final PixabayApi _api = PixabayApi();
 
   @override
-  Future<List<ImageItem>> getImageItem(String query) async {
+  Future<List<ImageItem>> getImageItems(String query) async {
     final PixabayDto dto = await _api.getImagesResult(query);
 
     if (dto.hits == null) {
