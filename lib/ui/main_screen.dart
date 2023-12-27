efec31a890af7320404898a23b9203b93cd763a4
shin-SeungHun <sng7387@gmail.com>
2023-12-28 00:02:03 +0900

    화면 UI 구현

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app/ui/widget/image_item_widget.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +11,41 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(width: 2, color: Colors.green),
+                  ),
+                  labelText: '검색',
+                  suffixIcon: IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.cyan,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
