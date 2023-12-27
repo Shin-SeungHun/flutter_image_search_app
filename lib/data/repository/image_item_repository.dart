11e41d3710e5c2649a54428992adb4f8de7497ed
shin-SeungHun <sng7387@gmail.com>
2023-12-27 23:07:53 +0900

    data source를 가져올 클래스 생성, 인터페이스 implements

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -1,7 +1,18 @@
+import 'package:image_search_app/data/data_source/pixabay_api.dart';
 import '../model/image_item.dart';
 
 abstract interface class ImageItemRepository {
   Future<List<ImageItem>> getImageItem(String query);
 }
 
+class PixabayImageItemRepository implements ImageItemRepository {
+  final PixabayApi _api = PixabayApi();
 
+  @override
+  Future<List<ImageItem>> getImageItem(String query) {
+    // TODO: implement getImageItem
+    throw UnimplementedError();
+  }
+
+
+}
